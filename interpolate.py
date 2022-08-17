2c0cdfe2f15f2a864ee4c59e27c4ef0df5e70a62
Eric Zhu <ezhu2009@gmail.com>
2022-08-17 11:07:36 -0400

    Undistort and camera stabilization

--- interpolate.py
+++ interpolate.py
@@ -1,51 +1,53 @@
-import cv2 as cv
+import string
 import numpy as np
-import glob
-import csv
-from scipy import interpolate
 from scipy.spatial.transform import Slerp, Rotation as R
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
 
-def get_interpolation():
-    position_file = pd.read_csv('/home/tau/Desktop/monocular_data/dataset-corridor1_512_16/mav0/mocap0/data.csv')
-    x_interp, y_interp, z_interp = \
-        interpolate.interp1d(position_file['#timestamp [ns]'], position_file[' p_RS_R_x [m]']), \
-        interpolate.interp1d(position_file['#timestamp [ns]'], position_file[' p_RS_R_y [m]']), \
-        interpolate.interp1d(position_file['#timestamp [ns]'], position_file[' p_RS_R_z [m]'])
 
+def get_interpolation(path:string):
+    position_file = pd.read_csv(path + '/mav0/imu0/data.csv')
+    time_diff_secs = np.array(position_file['#timestamp [ns]'])[1:] - np.array(position_file['#timestamp [ns]'])[0:-1]
+    time_diff_secs = np.array(time_diff_secs, dtype=float)
+    time_diff_secs /= 10.0**9
 
-    rotations = R.from_quat(position_file.loc[:, [' q_RS_x []', ' q_RS_y []', ' q_RS_z []', ' q_RS_w []']])
+    def quat_mult(q, p):
+        r = np.array([0.0, 0.0, 0.0, 0.0], dtype=np.float32)
+        r[0] = q[0]*p[0] - q[1]*p[1] - q[2]*p[2] - q[3]*p[3]
+        r[1] = q[0]*p[1] + q[1]*p[0] + q[2]*p[3] - q[3]*p[2]
+        r[2] = q[0]*p[2] - q[1]*p[3] + q[2]*p[0] + q[3]*p[1]
+        r[3] = q[0]*p[3] + q[1]*p[2] - q[2]*p[1] + q[3]*p[0]
+        return r
+    # Scalar first
+    # Forward eular, could do trapezoidal
+    def integrate_quaternion(q, gyr, dt):
+        p = np.array([0.0, gyr[0], gyr[1], gyr[2]], dtype=np.float32)
+        dot_q = 0.5 * quat_mult(q, p)
+        q_unpacked = q + dt * dot_q
+        return q_unpacked
+    
+    quad_list = [[1.0, 0.0, 0.0, 0.0]]
+
+    i = 0
+    for delta_time in time_diff_secs:
+        quad_list.append(integrate_quaternion(\
+            quad_list[-1], \
+            (position_file['w_RS_S_x [rad s^-1]'][i], position_file['w_RS_S_y [rad s^-1]'][i], position_file['w_RS_S_z [rad s^-1]'][i]), \
+            delta_time))
+        i += 1
+    
+    quad_array = np.array(quad_list)
+    quad_array = np.stack((quad_array[:, 1], quad_array[:, 2], quad_array[:, 3], quad_array[:, 0]), axis=1)
+
+    # print(quad_list)
+    rotations = R.from_quat(quad_array)
     quaternion_interp = Slerp(position_file['#timestamp [ns]'], rotations)
-    return (x_interp, y_interp, z_interp, quaternion_interp)
+    return quaternion_interp
 
 if __name__ == '__main__':
-    position_file = pd.read_csv('/home/tau/Desktop/monocular_data/dataset-corridor1_512_16/mav0/mocap0/data.csv')
-    x_interp, y_interp, z_interp = \
-        interpolate.interp1d(position_file['#timestamp [ns]'], position_file[' p_RS_R_x [m]']), \
-        interpolate.interp1d(position_file['#timestamp [ns]'], position_file[' p_RS_R_y [m]']), \
-        interpolate.interp1d(position_file['#timestamp [ns]'], position_file[' p_RS_R_z [m]'])
-
-
-    rotations = R.from_quat(position_file.loc[:, [' q_RS_x []', ' q_RS_y []', ' q_RS_z []', ' q_RS_w []']])
-    quaternion_interp = Slerp(position_file['#timestamp [ns]'], rotations)
-
-    rand_times = np.sort(np.random.rand(1000)) * (position_file['#timestamp [ns]'][len(position_file['#timestamp [ns]'])-1] - position_file['#timestamp [ns]'][0]) \
-        + position_file['#timestamp [ns]'][0]
-    print(quaternion_interp(rand_times).as_quat())
-    # plt.plot(rand_times, np.array(quaternion_interp(rand_times).as_quat())[:,0])
-    # plt.plot(position_file['#timestamp [ns]'], position_file[' q_RS_w []'])
-    # plt.plot(position_file['#timestamp [ns]'], position_file[' q_RS_x []'])
-    # plt.plot(position_file['#timestamp [ns]'], position_file[' q_RS_y []'])
-    # plt.plot(position_file['#timestamp [ns]'], position_file[' q_RS_z []'])
-    plt.plot(position_file['#timestamp [ns]'])
-
-
-
-    plt.show()
     
+    get_interpolation('/home/tau/Desktop/monocular_data/dataset-corridor4_512_16')
     
     
 
--- undistort.py
+++ undistort.py
@@ -1,95 +1,143 @@
-from math import cos, sin
+from copy import deepcopy
+import string
 import cv2 as cv
 import numpy as np
 import glob
-import csv
-from scipy.spatial.transform import Rotation as R
 from interpolate import get_interpolation
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
-def derotate_image(frame, K, rot_mat):
-    R_fc_to_c = rot_mat.T
-    #R_c_to_fc = DCM(q=q_c_to_fc)
-    #R_fc_to_c = R_c_to_fc.A.transpose()
-
-    # Derive this by considering p1 = (K R K_inv) (Z(X)/Z(RX)) p0
-    top_two_rows = (K @ R_fc_to_c @ np.linalg.inv(K))[0:2, :]
-    bottom_row = (R_fc_to_c @ np.linalg.inv(K))[2, :]
-
-    map_pixel_c_to_fc = np.vstack((top_two_rows, bottom_row))
-    map_pixel_c_to_fc_opencv = np.float32(map_pixel_c_to_fc.flatten().reshape(3,3))
-
-    frame_derotated = cv.warpPerspective(frame, map_pixel_c_to_fc_opencv, (frame.shape[1], frame.shape[0]), flags=cv.WARP_INVERSE_MAP+cv.INTER_LINEAR)
-    return frame_derotated
-
-#equidistant_params = [fx fy cx cy k1 k2 k3 k4] #from camera.txt file in dataset
-p = [0.373004838186, 0.372994740336, 0.498890050897, 0.502729380663, 0.00348238940225, 0.000715034845216, -0.00205323614187, 0.000202936735918]
-distCoeff = np.array([[0.00348238940225, 0.000715034845216, -0.00205323614187, 0.000202936735918]])
-directory = '/home/tau/Desktop/monocular_data/dataset-corridor1_512_16/dso/cam0/derotated_images/'
-
-images = []
-for image in glob.glob("/home/tau/Desktop/monocular_data/dataset-corridor1_512_16/dso/cam0/images/*.png"):
-    images.append(image)
-images.sort()
-frame_times = pd.read_csv('/home/tau/Desktop/monocular_data/dataset-corridor1_512_16/mav0/cam0/data.csv')
-(x_interp, y_interp, z_interp, rot_interp) = get_interpolation()
-i = 0
-global_trans_rot_mat = None
-
-
-plt.ion()
-for image in images:
-    image = cv.imread(image, 0)
-    height,width = image.shape
-    camera_matrix = np.array([[width*p[0] , 0 , width*p[2]-0.5] , [0 ,height*p[1], height*p[3]-0.5] , [0,0,1] ])
-    size_factor = 1
-    K_new = np.copy(camera_matrix)
-    K_new[0, 2] *= size_factor
-    K_new[1, 2] *= size_factor
-    undistorted = cv.fisheye.undistortImage(image, camera_matrix, distCoeff,Knew=K_new,new_size=(size_factor*width, size_factor*height))
+def undistort(path:string):
     
-    start_rot_mat = rot_interp(frame_times['#timestamp [ns]'][0]).as_matrix()
-    start_rot_mat = np.array(start_rot_mat)
+    contents = open(path + '/dso/cam0/camera.txt', "r").read().split(' ')
+    contents[8] = contents[8].split('\n')[0]
+    camera_params = contents[1:9]
+    # print(camera_params)
+    camera_params = [float(val) for val in camera_params]
+    distCoeff = np.array([camera_params[-4], camera_params[-3], camera_params[-2], camera_params[-1]])
+    images = []
+    for image in glob.glob(path + '/dso/cam0/images/*.png'):
+        images.append(image)
+    images.sort()
+    frame_times = pd.read_csv(path + '/mav0/cam0/data.csv')
+    rot_interp = get_interpolation(path)
+    i = 0
 
-    end_rot_mat = rot_interp(frame_times['#timestamp [ns]'][i]).as_matrix()
-    end_rot_mat = np.array(end_rot_mat)
-
-    trans_rot_mat =  np.linalg.inv(start_rot_mat) @ end_rot_mat
-    trans_rot_mat =  end_rot_mat @ np.linalg.inv(start_rot_mat)
-
-    Rcr = np.array(((-1, 0, 0),
-                    (0, 0, -1),
-                    (0, -1, 0)))
-    # Rcr = np.eye(3)
-
-    trans_rot_mat = Rcr @ trans_rot_mat @ Rcr.T
-
-    # plt.clf()
-    # plt.xlim([-1, 1])
-    # plt.ylim([-1, 1])
-    # # plt.gca().invert_yaxis()
-    # plt.plot([0, trans_rot_mat[0][0]/ (trans_rot_mat[2][0]+1)], [0, trans_rot_mat[1][0]/ (trans_rot_mat[2][0]+1)], 'r-')
-    # plt.plot([0, trans_rot_mat[0][1]/ (trans_rot_mat[2][1]+1)], [0, trans_rot_mat[1][1]/ (trans_rot_mat[2][1]+1)], 'g-')
-    # plt.plot([0, trans_rot_mat[0][2]/ (trans_rot_mat[2][2]+1)], [0, trans_rot_mat[1][2]/ (trans_rot_mat[2][2]+1)], 'b-')
-    
-    # print((trans_rot_mat[2][2]+1))
-    # print(np.linalg.det(trans_rot_mat))
-    plt.pause(0.01)         
-
-    
-    # trans_rot_mat = np.linalg.inv(trans_rot_mat)
+    mcap_start_time = pd.read_csv(path + '/mav0/mocap0/data.csv')['#timestamp [ns]'][0]
+    frame_start_time = pd.read_csv(path+'/mav0/cam0/data.csv')['#timestamp [ns]'][0]
+    imu_start_time = pd.read_csv(path + '/mav0/imu0/data.csv')['#timestamp [ns]'][0]
     
 
-    # cv.imshow('original', image)
-    
-    # undistorted = undistorted[:, ::-1]
+    start_time = max(mcap_start_time, frame_start_time, imu_start_time)
 
-    cv.imshow('undistorted',undistorted)
-    
-    
-    cv.imshow('derotated',derotate_image(undistorted, K_new, trans_rot_mat))
-    # cv.imwrite(directory + str(i) +"derotated.png" , output)
-    cv.waitKey(50)
-    i+=1
+    start_index = 0
+    while frame_times['#timestamp [ns]'][start_index] < start_time:
+        start_index += 1
+        images.remove(images[0])
+
+    plt.ion()
+    for image in images:
+        def derotate_image(frame, K, rot_mat):
+            R_fc_to_c = rot_mat.T
+            #R_c_to_fc = DCM(q=q_c_to_fc)
+            #R_fc_to_c = R_c_to_fc.A.transpose()
+
+            # Derive this by considering p1 = (K R K_inv) (Z(X)/Z(RX)) p0
+
+            print(frame.shape)
+
+            frame_for_derotation = np,deepcopy(frame)
+
+            print(frame_for_derotation)
+
+
+            derotated_pixels = R_fc_to_c @ np.linalg.inv(K) @ frame_for_derotation
+
+            # frame(np.where(derotated_pixels[:2]==0)) = 0
+
+            top_two_rows = (K @ R_fc_to_c @ np.linalg.inv(K))[0:2, :]
+            bottom_row = (R_fc_to_c @ np.linalg.inv(K))[2, :]
+
+            
+
+            map_pixel_c_to_fc = np.vstack((top_two_rows, bottom_row))
+            map_pixel_c_to_fc_opencv = np.float32(map_pixel_c_to_fc.flatten().reshape(3,3))
+
+            frame_derotated = cv.warpPerspective(frame, map_pixel_c_to_fc_opencv, (frame.shape[1], frame.shape[0]), flags=cv.WARP_INVERSE_MAP+cv.INTER_LINEAR)
+            return frame_derotated
+
+        image = cv.imread(image, 0)
+        height,width = image.shape
+        camera_matrix = np.array([[width*camera_params[0] , 0 , width*camera_params[2]-0.5] , [0 ,height*camera_params[1], height*camera_params[3]-0.5] , [0,0,1] ])
+        size_factor = 1
+        K_new = np.copy(camera_matrix)
+        K_new[0, 2] *= size_factor
+        K_new[1, 2] *= size_factor
+        undistorted = cv.fisheye.undistortImage(image, camera_matrix, distCoeff,Knew=K_new,new_size=(size_factor*width, size_factor*height))
+        
+        start_rot_mat = rot_interp(frame_times['#timestamp [ns]'][start_index]).as_matrix()
+        start_rot_mat = np.array(start_rot_mat)
+
+        end_rot_mat = rot_interp(frame_times['#timestamp [ns]'][start_index + i]).as_matrix()
+        end_rot_mat = np.array(end_rot_mat)
+
+        trans_rot_mat =  np.linalg.inv(start_rot_mat) @ end_rot_mat
+        # trans_rot_mat =  end_rot_mat @ np.linalg.inv(start_rot_mat)
+
+        Rcr = np.array(((-1, 0, 0),
+                        (0, 0, -1),
+                        (0, -1, 0)))
+
+        # Rcr = np.eye(3)
+        # Rcr = Rcr.T
+
+        trans_rot_mat = Rcr @ trans_rot_mat @ Rcr.T
+
+        plt.clf()
+        plt.xlim([-1, 1])
+        plt.ylim([-1, 1])
+        # # plt.gca().invert_yaxis()
+        plt.plot([0, trans_rot_mat[0][0]/ (trans_rot_mat[2][0]+1)], [0, -trans_rot_mat[1][0]/ (trans_rot_mat[2][0]+1)], 'r-')
+        plt.plot([0, trans_rot_mat[0][1]/ (trans_rot_mat[2][1]+1)], [0, -trans_rot_mat[1][1]/ (trans_rot_mat[2][1]+1)], 'g-')
+        plt.plot([0, trans_rot_mat[0][2]/ (trans_rot_mat[2][2]+1)], [0, -trans_rot_mat[1][2]/ (trans_rot_mat[2][2]+1)], 'b-')
+        
+        # print((trans_rot_mat[2][2]+1))
+        # print(np.linalg.det(trans_rot_mat))
+        plt.pause(0.01)         
+        plt.show()
+        
+        # trans_rot_mat = np.linalg.inv(trans_rot_mat)
+        
+
+        # cv.imshow('original', image)
+        
+        # undistorted = undistorted[:, ::-1]
+
+        cv.imshow('undistorted',undistorted)
+        
+        cv.imshow('derotated',derotate_image(undistorted, K_new, trans_rot_mat))
+        # cv.imwrite(directory + str(i) +"derotated.png" , output)
+        cv.waitKey(40)
+        break
+        i+=1
+
+if __name__ == '__main__':
+    # path = '/home/tau/Desktop/monocular_data/dataset-corridor3_512_16'
+
+    # imu_gt = np.array(pd.read_csv(path + '/dso/gt_imu.csv')['# timestamp[ns]'])
+    # imu_gt = (imu_gt - imu_gt[0]) / 10**9
+    # imu_gt_diff = imu_gt[1:] - imu_gt[0:-1]
+
+    # frames_times = np.array(pd.read_csv(path + '/mav0/cam0/data.csv')['#timestamp [ns]'])
+    # frames_times = (frames_times - frames_times[0]) / 10**9
+    # frames_diff = frames_times[1:] - frames_times[0:-1]
+
+    # imu = np.array(pd.read_csv(path + '/mav0/imu0/data.csv')['#timestamp [ns]'])
+    # imu = (imu - imu[0]) / 10**9
+    # imu_diff = imu[1:] - imu[0:-1]
+
+    # plt.plot(imu_gt_diff)
+    # plt.plot(frames_diff)
+    # plt.plot(imu_diff)
+    # plt.show()
+    undistort('/home/tau/Desktop/monocular_data/dataset-corridor1_512_16')
